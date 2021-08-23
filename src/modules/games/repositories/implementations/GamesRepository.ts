7275e804365a008fa522ac992d47a4423b55c631
William <macedo.franca@gmail.com>
2021-08-23 16:07:26 -0300

    Challenge finished.

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,23 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
+    .createQueryBuilder('games')
+    .where('games.title ilike :title', {title: `%${param}%`})
+    .getMany();
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('select count(id) from games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
+      .where(id)
+      .relation('users')
+      .of(id)
+      .loadMany();
       // Complete usando query builder
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,20 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
+    return this.repository.findOneOrFail({where: { id: user_id }, relations: ["games"]});
     // Complete usando ORM
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM USERS us ORDER BY us.first_name'); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      'SELECT * FROM USERS usuario WHERE LOWER(usuario.first_name) = $1 AND LOWER(usuario.last_name) = $2', 
+      [first_name.toLowerCase(), last_name.toLowerCase()]); // Complete usando raw query
   }
 }
